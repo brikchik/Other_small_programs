1b01e9807b4eb7ecb6bdfcd835424f394329cf13
javister <javist.gr@mail.ru>
2017-06-14 03:00:18 +0500

    stack of class with copying

---
+++ Task 3/Task 3/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Task_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button10 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(161, 13);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(368, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Задать дату";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(161, 46);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(200, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(368, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Задать номер";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(161, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(368, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Задать текст";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 24);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(110, 251);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(122, 286);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выбор стека";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(6, 18);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(194, 95);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(213, 18);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(66, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Номер";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(213, 47);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(136, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Сообщение";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(285, 18);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(64, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Дата";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(213, 90);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(136, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Вся информация";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(161, 117);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(200, 23);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "Добавить новый элемент";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(161, 146);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(200, 23);
+            this.button9.TabIndex = 14;
+            this.button9.Text = "Удалить элемент";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Location = new System.Drawing.Point(161, 175);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(359, 124);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Текущий элемент";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(19, 314);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(501, 23);
+            this.button10.TabIndex = 16;
+            this.button10.Text = "скопировать стек2 в стек 1";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 349);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ Task 3/Task 3/MessageStack.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Task_3
+{
+    class Message
+    {
+        public Exception MessageException = new Exception("Регистрировать сообщения из далекого будущего нельзя");
+        public Message(DateTime date,uint number=0,string message="empty")
+        {
+            setNumber(number);
+            setMessage(message);
+            setDate(date);
+        } //все варианты с датой
+        public Message(uint number = 0, string message = "empty")
+        {
+            setNumber(number);
+            setMessage(message);
+            setDate(DateTime.Now);
+        } // учитывает все варианты без даты, датой становится текущее время(нельзя передать текущее время по умолчанию)
+        ~Message() { }
+        private uint number;
+        private string message;
+        private DateTime date;
+        public uint getNumber() { return number; }
+        public string getMessage() { return message; }
+        public DateTime getDate() { return date; }
+        public void setNumber(uint x) { number=x; } //ограничений кроме беззнаковости не накладывается
+        public void setMessage(string s) { message=s; } //ограничений тоже нет, вдруг нужен элемент с пустой строкой
+        public void setDate(DateTime date)//обязательные ограничения корректности 
+            //(конечно, DateTime проверит корректность, остаётся только убедиться, что нет слишком старых)
+        {
+            if ( ( date.Year - DateTime.Today.Year) < 5) this.date = date; else throw MessageException;
+        }
+        public string getFullMessage()
+        {
+            string Fstring="";
+            Fstring += "Number = " + number + "; ";
+            Fstring += "Message = " + message + "; ";
+            Fstring += "Date = " + date + ".";
+            return Fstring;
+        }
+    }
+    class MessageStack
+    {
+        //получается слишком простая реализация из-за наличия готового примитива Stack<тип объекта>
+        //но писать интерфейс на WinAPI в C++ очень уж трудоёмко
+        //а использовать адресацию мешает требование безопасной среды(которую не понял, как выключить)
+        public static Exception StackEmpty=new Exception("Stack empty. Unable to get element");
+        public static Exception NotEnoughMemory = new Exception("Stack is too big. Unable to insert element");
+        private Stack<Message> stack=new Stack<Message>();
+        public MessageStack() { }//пустой стек
+        public MessageStack(int count)
+        {
+            for (int i = 0; i < count; i++) push(new Message());
+        }//если понадобится гарантированно выделить нужный объём памяти
+        public void push(Message element)
+        {
+            try { stack.Push(element); }
+            catch (Exception x) { throw NotEnoughMemory; }
+            //исключения типа "повреждение стека" не рассмотрены, т.к. в методах и так есть проверки;
+            //нет прямой работы с элементами извне
+        }
+        public Message pop()
+        {
+            if (isEmpty()) throw StackEmpty;
+            return stack.Pop();
+        }
+        public Message peek()
+        {
+            if (isEmpty()) throw StackEmpty;
+            return stack.Peek();
+        }
+        public bool isEmpty()
+        {
+            bool empty = false;
+            if (stack.Count == 0) empty = true;
+            return empty;
+        }
+        public int getSize() { return stack.Count; }
+        public static bool operator&(MessageStack a, MessageStack b)//вместо непереопределяемого =
+        {
+            for (int i = 0; i < a.getSize(); i++) a.pop();
+            MessageStack x=new MessageStack();
+            for (int i = 0; i < b.getSize(); i++)
+            {
+                DateTime date = b.peek().getDate();
+                uint number = b.peek().getNumber();
+                string message = b.peek().getMessage();
+                b.pop();
+                x.push(new Message(date, number, message));
+            }//теперь в x весь b наоборот
+            for (int i = 0; i < x.getSize(); i++)
+            {
+                DateTime date = x.peek().getDate();
+                uint number = x.peek().getNumber();
+                string message = x.peek().getMessage();
+                x.pop();
+                a.push(new Message(date, number, message));
+                b.push(new Message(date, number, message));
+            }//специфика в том, что стек приходится "переворачивать"->лишняя операция
+            return true;
+        }
+    }
+}
