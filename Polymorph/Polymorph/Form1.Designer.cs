967f611ba44acc49eac5e38a1fbd1bc1f1af24a5
javister <javist.gr@mail.ru>
2017-06-21 01:31:41 +0500

    temperature graphic indicator

---
+++ Polymorph/Polymorph/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Polymorph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(22, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(273, 207);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(366, 261);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(304, 214);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(22, 248);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(323, 227);
+            this.panel3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(441, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(229, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "set Temperature";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(312, 23);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(123, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(312, 69);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(123, 20);
+            this.numericUpDown2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(441, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(229, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "set Temperature";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(312, 115);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(123, 20);
+            this.numericUpDown3.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(441, 112);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(229, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "set Temperature";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 487);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Polymorph/Polymorph/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+namespace Polymorph
+{
+
+    public abstract class temperatureIndicator
+    {
+        protected int color;
+        protected float brightness;
+        protected float size;
+        public abstract void setTemp(float t);
+        public abstract float getTemp();
+        public abstract int getColor();
+        public abstract void setColor();
+        public abstract float getBrightness();
+        public abstract void setBrightness(float b);
+        public abstract void setSize(float a, float b);
+        public abstract float getSize();
+        public abstract void doPaint();
+    }
+    public class temp1 : temperatureIndicator
+    {
+        Graphics grBack;
+        Panel panel;
+        float temp=0;
+        public temp1(Panel p, Graphics c) { panel = p; grBack = c; }
+        public override void setTemp(float t)
+        {
+            temp = t;
+        }
+        public override float getTemp() { return temp; }
+        public override int getColor() { return 0; }
+        public override void setColor() { }
+        public override float getBrightness() { return 0; }
+        public override void setBrightness(float a) { }
+        public override void setSize(float a, float b) { }
+        public override float getSize() { return 0; }
+
+        public override void doPaint()
+        {
+            Bitmap btmBack = new Bitmap(348, 235);      //изображение
+            grBack = Graphics.FromImage(btmBack);
+            panel.BackgroundImage = btmBack;
+            grBack.FillEllipse(Brushes.Bisque, new Rectangle(20, 20, 80, 80));
+            grBack.DrawString(this.getTemp()+"°C", new Font(FontFamily.GenericMonospace, 15), Brushes.Blue, new Point(90, 10));
+            panel.Refresh();
+            Thread.Sleep(50);
+        }
+    }
+    public class temp2 : temperatureIndicator
+    {
+        Panel panel;
+        Graphics grBack;
+        float temp=0;
+        public temp2(Panel p , Graphics c) { panel = p; grBack = c; }
+        public override void setTemp(float t)
+        {
+            temp = t;
+        }
+        public override float getTemp() { return temp; }
+        public override int getColor() { return 0; }
+        public override void setColor() { }
+        public override float getBrightness() { return 0; }
+        public override void setBrightness(float a) { }
+        public override void setSize(float a, float b) { }
+        public override float getSize() { return 0; }
+        public override void doPaint()
+        {
+            Bitmap btmBack = new Bitmap(348, 235);
+            Graphics grBack = Graphics.FromImage(btmBack);
+            panel.BackgroundImage = btmBack;
+            grBack.DrawRectangle(Pens.Brown, new Rectangle(20, 20, 100, 25));
+            grBack.DrawString("T1 = " + this.getTemp() + " °C", new Font(FontFamily.GenericSansSerif, 13), Brushes.Blue, new Point(22, 23));
+            panel.Refresh();
+            Thread.Sleep(50);
+        }
+    }
+    public class temp3 : temperatureIndicator
+    {
+        Graphics grBack;
+        float temp=0;
+        Panel panel;
+        public temp3(Panel p, Graphics c) { panel = p; grBack = c; }
+        public override void setTemp(float t)
+        {
+            temp = t;
+        }
+        public override float getTemp() { return temp; }
+        public override int getColor() { return 0; }
+        public override void setColor() { }
+        public override float getBrightness() { return 0; }
+        public override void setBrightness(float a) { }
+        public override void setSize(float a, float b) { }
+        public override float getSize() { return 0; }
+        public override void doPaint()
+        {
+            Bitmap btmBack = new Bitmap(348, 235);      //изображение
+            Graphics grBack = Graphics.FromImage(btmBack);
+            panel.BackgroundImage = btmBack;
+            grBack.DrawRectangle(Pens.Black, 20, 20, 50, 200);
+            grBack.FillRectangle(Brushes.Blue, 60, 200 - this.getTemp() * 4, 5, this.getTemp() * 4);
+            grBack.DrawString(this.getTemp()+"C", new Font(FontFamily.GenericSansSerif, 12), Brushes.Black, 80, 40);
+            grBack.DrawLine(Pens.Black, 60, 30, 60, 200);
+            grBack.DrawLine(Pens.Black, 50, 200, 70, 200);
+            grBack.DrawString("0", new Font(FontFamily.GenericSansSerif, 11), Brushes.Black, 40, 200);
+            grBack.DrawLine(Pens.Black, 50, 160, 70, 160);
+            grBack.DrawString("10", new Font(FontFamily.GenericSansSerif, 11), Brushes.Black, 35, 160);
+            grBack.DrawLine(Pens.Black, 50, 120, 70, 120);
+            grBack.DrawString("20", new Font(FontFamily.GenericSansSerif, 11), Brushes.Black, 35, 120);
+            grBack.DrawLine(Pens.Black, 50, 80, 70, 80);
+            grBack.DrawString("30", new Font(FontFamily.GenericSansSerif, 11), Brushes.Black, 35, 80);
+            grBack.DrawLine(Pens.Black, 50, 40, 70, 40);
+            grBack.DrawString("40", new Font(FontFamily.GenericSansSerif, 11), Brushes.Black, 35, 40);
+            panel.Refresh();
+            Thread.Sleep(50);
+        }
+    }
+    public partial class Form1 : Form
+    {
+        public temp1 t1;
+        public temp2 t2;
+        public temp3 t3;
+        public Form1()
+        {
+            InitializeComponent();
+            t1 = new temp1(panel1,panel1.CreateGraphics());
+            t2 = new temp2(panel2,panel2.CreateGraphics());
+            t3 = new temp3(panel3,panel3.CreateGraphics());
+            //Thread x = new Thread(t1.doPaint);
+            //x.Start();
+            //Thread y = new Thread(t2.doPaint);
+            //y.Start();
+            //Thread z = new Thread(t3.doPaint);
+            //z.Start();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e){}
+        private void panel2_Paint(object sender, PaintEventArgs e){}
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            t1.setTemp((float)numericUpDown1.Value);
+            t1.doPaint();
+        }
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            t2.setTemp((float)numericUpDown2.Value);
+            t2.doPaint();
+        }
+        private void button3_Click(object sender, System.EventArgs e)
+        {
+            t3.setTemp((float)numericUpDown3.Value);
+            t3.doPaint();
+        }
+    }
+}
